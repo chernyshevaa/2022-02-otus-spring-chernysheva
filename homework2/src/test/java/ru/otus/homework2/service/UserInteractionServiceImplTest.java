55d9326adac0a7c0b5e83ea46749da97369dfbcb
Anastasiya Chernysheva <anastasiya.chernysheva@soft-werke.com>
2022-03-20 23:38:56 +0300

    test fix

--- homework2/src/test/java/ru/otus/homework2/service/UserInteractionServiceImplTest.java
+++ homework2/src/test/java/ru/otus/homework2/service/UserInteractionServiceImplTest.java
@@ -12,37 +12,36 @@ import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @DisplayName("UserInteractionServiceImpl Test")
 @ExtendWith(MockitoExtension.class)
 public class UserInteractionServiceImplTest {
     private static final String QUESTION = "question";
     private static final String ANSWER = "answer";
-    @Mock
-    PrintStream printStream;
 
     @Mock
-    InputStream inputStream;
+    private IOService ioService;
 
     @DisplayName("Print message test")
     @Test
     public void shouldPrintMessage() {
-        UserInteractionServiceImpl userInteractionService = new UserInteractionServiceImpl(printStream, inputStream);
+        UserInteractionServiceImpl userInteractionService = new UserInteractionServiceImpl(ioService);
 
         userInteractionService.printMessage(QUESTION);
 
-        verify(printStream).println(QUESTION);
+        verify(ioService).outputString(QUESTION);
     }
 
     @DisplayName("Ask string question test")
     @Test
     public void shouldAskQuestionString() {
-        InputStream answerInputStream = new ByteArrayInputStream(ANSWER.getBytes());
-        UserInteractionServiceImpl userInteractionService = new UserInteractionServiceImpl(printStream, answerInputStream);
+        UserInteractionServiceImpl userInteractionService = new UserInteractionServiceImpl(ioService);
+        when(ioService.readString()).thenReturn(ANSWER);
 
         String answer = userInteractionService.askQuestion(QUESTION);
 
-        verify(printStream).println(QUESTION);
+        verify(ioService).outputString(QUESTION);
         assertEquals(ANSWER, answer);
     }
 }
